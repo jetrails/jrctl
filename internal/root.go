e886d57aa4395a80bfc1d82351ca34bf212f08c2
Rafael Grigorian <me@raffi.io>
2020-06-24 00:04:38 -0500

    Fixed #3

--- internal/root.go
+++ internal/root.go
@@ -2,17 +2,32 @@ package internal
 
 import "fmt"
 import "os"
+import "encoding/json"
+import "github.com/jetrails/jrctl/sdk/utils"
 import "github.com/spf13/cobra"
 import "github.com/spf13/viper"
 import "github.com/fatih/color"
+import "github.com/parnurzeal/gorequest"
+
+const Version = "1.0.1"
+const ReleasesUrl = "https://api.github.com/repos/jetrails/jrctl/releases"
+const TagUrlTemplate = "https://github.com/jetrails/jrctl/releases/tag/%s"
 
 var configFile string
 var jetrails = color.GreenString (">") + "jetrails" + color.GreenString ("_")
 
+type Release struct {
+	TagName string `json:"tag_name"`
+}
+
+type ReleaseResponse struct {
+	Collection [] Release
+}
+
 var rootCmd = &cobra.Command {
 	Use:   "jrctl",
 	Short: "Command line tool to help interact with the " + jetrails + " API",
-	Version: "1.0.1",
+	Version: Version,
 }
 
 func Execute () {
@@ -24,6 +39,39 @@ func Execute () {
 
 func init () {
 	cobra.OnInitialize ( initConfig )
+	checkVersion ()
+}
+
+func checkVersion () {
+	var cacheWindow int64 = 60 * 60
+	cachedVersion, hit := utils.GetCache ( "version:" + Version, cacheWindow )
+	if hit {
+		if Version != cachedVersion {
+			fmt.Printf (
+				"Software is out-of-date. Update to the latest version: %s.\n%s\n\n",
+				cachedVersion,
+				color.GreenString ( fmt.Sprintf ( TagUrlTemplate, cachedVersion ) ),
+			)
+		}
+		return
+	}
+	var request = gorequest.New ()
+	var debug = viper.GetBool ("debug")
+	request.SetDebug ( debug )
+	response, body, _ := request.Get ( ReleasesUrl ).End ()
+	if response.StatusCode == 200 {
+		releases := make ( [] Release, 0 )
+		json.Unmarshal ( [] byte ( body ), &releases )
+		newest := releases [ 0 ]
+		utils.SetCache ( "version:" + Version, newest.TagName, cacheWindow )
+		if Version != newest.TagName {
+			fmt.Printf (
+				"Software is out-of-date. Update to the latest version: %s.\n%s\n\n",
+				newest.TagName,
+				color.GreenString ( fmt.Sprintf ( TagUrlTemplate, newest.TagName ) ),
+			)
+		}
+	}
 }
 
 func initConfig () {
---
+++ sdk/utils/cache.go
@@ -0,0 +1,33 @@
+package utils
+
+import "os"
+import "fmt"
+import "path"
+import "time"
+import "io/ioutil"
+
+func getCacheFileName ( key string, window int64 ) string {
+	now := time.Now ().Unix ()
+	postfix := now / window
+	return fmt.Sprintf ( "jrctl-%s-%d", key, postfix )
+}
+
+func SetCache ( key string, value string, window int64 ) string {
+	name := getCacheFileName ( key, window )
+	filepath := path.Join ( os.TempDir (), name )
+	file, _ := os.Create ( filepath )
+	_, _ = file.WriteString ( value )
+	file.Close ()
+	return filepath
+}
+
+func GetCache ( key string, window int64 ) ( string, bool ) {
+	name := getCacheFileName ( key, window )
+	filepath := path.Join ( os.TempDir (), name )
+	info, error := os.Stat ( filepath )
+	if os.IsNotExist ( error ) || info.IsDir () {
+		return "", false
+	}
+	contents, _ := ioutil.ReadFile ( filepath )
+	return string ( contents ), true
+}
